c49944c87d7f7f5527faa99b0e5b34d2b220d350
nscherer30 <nscherer30@gmail.com>
2020-12-02 14:54:21 -0800

    need to handle keys still, added more mini projects

---
+++ giphy-and-movies-search/giphy-and-movies-search.js
@@ -0,0 +1,131 @@
+var keywordContainer = document.getElementById("keyword-container");
+var contentContainer = document.getElementById("main-container");
+var mainContentContainer = document.getElementById("main-container");
+var queryHist = [];
+
+
+var searchBar = document.getElementById("search");
+var keywordCardContainer = document.getElementById("keyword-container");
+
+var createKeywordBtn = document.getElementById("create-keyword-btn");
+createKeywordBtn.addEventListener("click", () => {
+    createKeyWordCard(keywordCardContainer, searchBar.value);
+});
+
+
+function createKeyWordCard(containerParent, keyword) {
+
+    let outsideContainer = document.createElement("div");
+    outsideContainer.className = "col-xs-1-12";
+    let cardContainer = document.createElement("div");
+    cardContainer.className = "card";
+
+    outsideContainer.appendChild(cardContainer);
+
+    let cardBody = document.createElement("div");
+    cardBody.className = "card-body";
+
+    cardContainer.appendChild(cardBody);
+
+
+    let cardTitle = document.createElement("h3");
+    cardTitle.className = "card-title";
+    cardTitle.textContent = keyword;
+
+    let cardMovieBtn = document.createElement("button");
+    cardMovieBtn.innerHTML = "Movies";
+    cardMovieBtn.addEventListener("click", () => {
+        getMovies(cardTitle.textContent);
+    });
+
+    let cardGIFBtn = document.createElement("button");
+    cardGIFBtn.innerHTML = "GIFs";
+    cardGIFBtn.addEventListener("click", () => {
+        getGIF(cardTitle.textContent);
+    });
+
+    cardBody.appendChild(cardTitle);
+    cardBody.appendChild(cardMovieBtn);
+    cardBody.appendChild(cardGIFBtn);
+
+    containerParent.appendChild(outsideContainer);
+}
+
+
+function getMovies(query) {
+    removeAllChildNodes(mainContentContainer);
+    axios.get("http://www.omdbapi.com/?apikey=dbf8339d&type=movie&s=" + query).then(function (res) {
+        let responseArray = res.data.Search;
+        for (let i = 0; i < 11; i++) {
+            let newMovieContainer = document.createElement("div");
+            newMovieContainer.className = "content-item";
+
+            let moviePosterSrcImg = responseArray[i].Poster;
+            let movieTitle = responseArray[i].Title;
+
+            let newMovieImg = document.createElement("img");
+            newMovieImg.src = moviePosterSrcImg;
+
+            let newMovieTitle = document.createElement("h3");
+            newMovieTitle.textContent = movieTitle;
+
+            newMovieContainer.appendChild(newMovieTitle);
+            newMovieContainer.appendChild(newMovieImg);
+            mainContentContainer.appendChild(newMovieContainer);
+        }
+    });
+
+}
+
+
+
+function getGIF(query) {
+    removeAllChildNodes(mainContentContainer);
+    let searchQuery;
+    if (query.length > 0) {
+        searchQuery = query;
+    } else {
+        searchQuery = searchBar.value;
+    }
+
+
+    if (!(queryHist.includes(searchQuery))) {
+        let newSearchBtn = document.createElement("button");
+        newSearchBtn.innerHTML = searchQuery;
+        newSearchBtn.addEventListener("click", function () {
+            getGIF(this.innerHTML);
+        });
+        queryHist.push(searchQuery);
+        document.body.prepend(newSearchBtn);
+    }
+
+    axios.get("http://api.giphy.com/v1/gifs/search?api_key=WqveMC9zydrTtIyrQ6XHFg3KZG8x2SJg&q=" + searchQuery).then(function (res) {
+        let contentArr = res.data.data;
+        contentArr.map(gif => {
+            const url = gif.images.downsized.url;
+            const desc = gif.title;
+
+            let newGIFContainer = document.createElement("div");
+
+            let newGIFImg = document.createElement("img");
+            newGIFImg.src = url;
+
+            let gifDesc = document.createElement("p");
+            gifDesc.innerHTML = desc;
+
+            newGIFContainer.className = "content-item";
+            newGIFContainer.appendChild(newGIFImg);
+            newGIFContainer.appendChild(gifDesc);
+
+
+            mainContentContainer.appendChild(newGIFContainer);
+        });
+
+    });
+}
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
---
+++ job-board/job-board.js
@@ -0,0 +1,125 @@
+var mainContentContainer = document.getElementById("main-container");
+var descriptionTextInput = document.getElementById("description-search");
+var locationTextInput = document.getElementById("location-search");
+var currentJobData = [];
+
+var filterFullTimeBtn = document.getElementById("filter-full-time-btn");
+filterFullTimeBtn.addEventListener("click", () => {
+    let updatedJobData = [...changeJobData("Full Time")];
+    refreshContent(updatedJobData);
+});
+
+var filterPartTimeBtn = document.getElementById("filter-part-time-btn");
+filterPartTimeBtn.addEventListener("click", () => {
+    let updatedJobData = [...changeJobData("Part Time")];
+    refreshContent(updatedJobData);
+});
+
+
+var findJobsBtn = document.getElementById("find-jobs-btn");
+findJobsBtn.addEventListener("click", () => {
+    let descQuery = descriptionTextInput.value;
+    let locQuery = locationTextInput.value;
+    if (isJobInputValid(descQuery, locQuery)) {
+        showJobs(descQuery, locQuery);
+    } else {
+        alert("Please enter a value for description, location or both.")
+    }
+});
+
+function showJobs(desc, location) {
+    let baseQuery = "https://jobs.github.com/positions.json?";
+    let description = "description=" + desc;
+    let concatter = "&";
+    let loc = "location=" + location;
+    let axiosQuery = baseQuery + description + concatter + loc;
+    axios.get(axiosQuery)
+        .then(function (res) {
+            let jobs = res.data;
+            currentJobData = [];
+            removeAllChildNodes(mainContentContainer);
+            jobs.map(job => {
+                createJobCard(mainContentContainer, job.company_logo, job.title, job.description, job.how_to_apply, job.url);
+                currentJobData.push(job);
+            });
+        })
+        .catch(err => console.log(err));
+}
+
+
+
+function changeJobData(filterType) {
+    let updatedJobDataContainer = [];
+    updatedJobDataContainer = currentJobData.filter(job => {
+        let isJobValid = (job.type === filterType);
+        return isJobValid;
+    });
+    return updatedJobDataContainer;
+}
+
+function refreshContent(newData) {
+    removeAllChildNodes(mainContentContainer);
+    newData.map(job => {
+        createJobCard(mainContentContainer, job.company_logo, job.title, job.description, job.how_to_apply, job.url);
+    });
+}
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function isJobInputValid(desc, loc) {
+    if (desc.length < 1 && loc.length < 1) {
+        return false;
+    }
+    return true;
+}
+
+function createJobCard(containerParent, companyImgSrc, jobTitle, jobDesc, jobHowToApply, jobApplicationLink) {
+
+    let outsideContainer = document.createElement("div");
+    outsideContainer.className = "col-xs-1-12";
+    outsideContainer.className = "content-item";
+    let cardContainer = document.createElement("div");
+    cardContainer.className = "card";
+
+    outsideContainer.appendChild(cardContainer);
+
+    let cardBody = document.createElement("div");
+    cardBody.className = "card-body";
+
+    cardContainer.appendChild(cardBody);
+
+    let jobCompanyImg = document.createElement("img");
+    jobCompanyImg.src = companyImgSrc;
+    jobCompanyImg.style.width = "15vw";
+    jobCompanyImg.style.height = "15vh";
+
+    let jobTitleH3 = document.createElement("h3");
+    jobTitleH3.className = "card-title";
+    jobTitleH3.textContent = jobTitle;
+
+    let jobDescP = document.createElement("p");
+    jobDescP.innerHTML = jobDesc.substring(0, 100) + "...";
+
+    let jobHowToApplyP = document.createElement("p");
+    jobHowToApplyP.innerHTML = jobHowToApply;
+
+    let jobApplicationLinkBtn = document.createElement("button");
+    jobApplicationLinkBtn.innerHTML = "Details";
+    jobApplicationLinkBtn.addEventListener("click", () => {
+        let fullDesc = jobDesc;
+        jobDescP.innerHTML = fullDesc;
+    });
+    jobApplicationLinkBtn.className = "btn btn-primary";
+
+    cardBody.appendChild(jobCompanyImg);
+    cardBody.appendChild(jobTitleH3);
+    cardBody.appendChild(jobDescP);
+    cardBody.appendChild(jobHowToApplyP);
+    cardBody.appendChild(jobApplicationLinkBtn);
+
+    containerParent.appendChild(outsideContainer);
+}
